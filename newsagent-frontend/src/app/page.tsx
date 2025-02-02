0476bf0a89a4f4d4db2ff7e2804252b9a4a65859
ejaz4 <ejazali4@protonmail.ch>
2025-02-02 11:36:16 +0000

    ⚡️ animation

--- newsagent-frontend/src/app/page.tsx
+++ newsagent-frontend/src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import styles from "./frontpage.module.css";
 import { MainHero } from "./_components/hero";
 import { Feed } from "./_components/feed";
 import { StarFeed } from "./_components/feed/starFeed";
@@ -23,22 +23,39 @@ export default function Home() {
         {/* <MiniDisplay larger={true} /> */}
       </div>
 
-      <div className="section">
-        <StarFeed channel="latest" />
-      </div>
-      <div className="section">
-        <h1>Latest</h1>
-        <Feed channel="latest" />
-      </div>
+      <div className={styles.mainContent}>
+        <div className="section">
+          <StarFeed channel="latest" />
+        </div>
+        <div className="section">
+          <h1>Latest</h1>
+          <Feed channel="latest" />
+        </div>
 
-      <div className="section">
-        <h1>Technology</h1>
-        <Feed channel="@category/Technology" />
-      </div>
+        <div className="section">
+          <h1>Politics</h1>
+          <Feed channel="@category/Politics" />
+        </div>
 
-      <div className="section">
-        <h1>Society</h1>
-        <Feed channel="@category/Society" />
+        <div className="section">
+          <h1>Technology</h1>
+          <Feed channel="@category/Technology" />
+        </div>
+
+        <div className="section">
+          <h1>Business</h1>
+          <Feed channel="@category/Business" />
+        </div>
+
+        <div className="section">
+          <h1>Sport</h1>
+          <Feed channel="@category/Sport" />
+        </div>
+
+        <div className="section">
+          <h1>Society</h1>
+          <Feed channel="@category/Society" />
+        </div>
       </div>
     </div>
   );
