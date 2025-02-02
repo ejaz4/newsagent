87075e27f1cf0ec1e46089c2f5e0287a04e6302b
ejaz4 <ejazali4@protonmail.ch>
2025-02-02 11:19:23 +0000

    🚑

--- newsagent-backend/src/index.ts
+++ newsagent-backend/src/index.ts
@@ -10,26 +10,33 @@ export const api = fastify({
   logger: true,
 }).withTypeProvider<TypeBoxTypeProvider>();
 
+const rss = [
+  "https://feeds.bbci.co.uk/news/rss.xml",
+  "https://feeds.skynews.com/feeds/rss/home.xml",
+  "https://www.theguardian.com/uk/rss",
+  "http://rss.cnn.com/rss/edition_world.rss",
+  "https://www.cnbc.com/id/100727362/device/rss/rss.html",
+  "https://abcnews.go.com/abcnews/internationalheadlines",
+  "https://www.aljazeera.com/xml/rss/all.xml",
+  "https://www.independent.co.uk/news/world/rss",
+  "https://www.telegraph.co.uk/news/rss.xml",
+  "https://www.ft.com/?format=rss",
+  "https://www.reutersagency.com/feed/?taxonomy=best-sectors&post_type=best",
+]
+
 const main = async () => {
-  await fetchRss("https://feeds.bbci.co.uk/news/rss.xml");
-  await fetchRss("https://feeds.skynews.com/feeds/rss/home.xml");
-  await fetchRss("https://www.theguardian.com/uk/rss");
-  await fetchRss("http://rss.cnn.com/rss/edition_world.rss");
-  await fetchRss("https://www.cnbc.com/id/100727362/device/rss/rss.html");
-  await fetchRss("https://abcnews.go.com/abcnews/internationalheadlines");
-  await fetchRss("https://www.aljazeera.com/xml/rss/all.xml");
-  await fetchRss("https://www.independent.co.uk/news/world/rss");
-  await fetchRss("https://openrss.org/www.thetimes.com");
-  await fetchRss("https://www.telegraph.co.uk/news/rss.xml");
-  await fetchRss("https://www.ft.com/?format=rss");
-  await fetchRss(
-    "https://www.reutersagency.com/feed/?taxonomy=best-sectors&post_type=best"
-  );
+  for (const url of rss) {
+    try {
+      await fetchRss(url);
+    } catch (e) {
+      console.error(e);
+    }
+  }
 
   while (true) {
     try {
       console.log(await generateArticles());
-      await setTimeout(1000 * 60 * 5);
+      await setTimeout(5000);
     } catch (e) {
       console.error(e);
     }
